13263b4bfbe1d5089bb66e527974843c328909d0
stretchinnovation <it.stretchinnovation@gmail.com>
2026-03-19 00:06:52 +0200

    Update AthleticStarters.py

--- AthleticStarters.py
+++ AthleticStarters.py
@@ -119,6 +119,19 @@ if files:
             margin-left: 0px;
             margin-right: 0px;
         }
+        .custom-table {
+            font-family: 'Poppins', sans-serif;
+            font-size: 1.5em;
+            font-weight: 800;
+            color: #000000 !important;
+            #padding: 6px 20px 12px 40px;
+            #width: 100vw;
+            text-transform: uppercase;
+            margin-top: 0em;
+            margin-bottom: 0em;
+            margin-left: 0px;
+            margin-right: 0px;
+        }
         </style>
         """,
         unsafe_allow_html=True
@@ -237,10 +250,8 @@ if files:
     st.html(
         f"""
     <div id="STARTERS">
-        <div class="custom-subheader">
-            {head}
-        </div>
-        {styled.to_html()}
+        <div class="custom-subheader">{head}</div>
+        <div class="custom-table">{styled.to_html()}</div>
     </div>
     """
     )
