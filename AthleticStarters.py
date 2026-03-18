925ca967896e6bb20e39795838ee3d363b029cf5
stretchinnovation <it.stretchinnovation@gmail.com>
2026-03-18 23:56:14 +0000

    css

--- AthleticStarters.py
+++ AthleticStarters.py
@@ -104,7 +104,7 @@ st.markdown(
         background-size: cover;
         background-repeat: no-repeat;
         padding: 6px 20px 10px 30px;
-        Width: 100vw;
+        #max-width: 1280px;
         text-transform: uppercase;
         margin-top: 0em;
         margin-bottom: 0em;
