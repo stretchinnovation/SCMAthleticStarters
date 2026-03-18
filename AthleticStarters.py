d5169660cf7eecf7ad851d777d570ed577fee602
stretchinnovation <it.stretchinnovation@gmail.com>
2026-03-18 23:59:25 +0200

    Update AthleticStarters.py

--- AthleticStarters.py
+++ AthleticStarters.py
@@ -148,7 +148,7 @@ if files:
                     ("margin", "0px"),
                     ("padding", "0px 0px"),
                     ("border-collapse", "collapse"),  # ensure borders collapse
-                    ("border-spacing", "0px")
+                    ("border", "none")
                 ]},
                 # Header styling 
                 {"selector": "th", "props": [
