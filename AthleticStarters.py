853cd704a016c0bb80cfb686b85f61f7df0bbf0c
stretchinnovation <it.stretchinnovation@gmail.com>
2026-03-19 00:37:13 +0000

    css

--- AthleticStarters.py
+++ AthleticStarters.py
@@ -104,7 +104,7 @@ st.markdown(
         background-image: url("app/static/KZNA_StartList_Title_Bar.png") !important;
         background-size: cover;
         background-repeat: no-repeat;
-        padding: 6px 20px 10px 30px;
+        padding: 6px 0px 10px 30px;
         #width: 100%;
         text-transform: uppercase;
         margin-top: 0em;
