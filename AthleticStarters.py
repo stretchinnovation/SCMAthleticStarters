3606321035c264b0d5934e088900083210f79a42
stretchinnovation <it.stretchinnovation@gmail.com>
2026-03-19 00:57:47 +0000

    vw

--- AthleticStarters.py
+++ AthleticStarters.py
@@ -139,7 +139,7 @@ styled = (
         [
             # Table dimensions
             {"selector": "table", "props": [
-                ("width", "100% !important"),
+                ("width", "100vw !important"),
                 ("max-height", "240px"),
                 ("margin", "0px"),
                 ("padding", "0px 0px"),
