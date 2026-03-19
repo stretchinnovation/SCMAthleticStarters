d81be208b23afbe75e50641cabe15afd5b5c48f4
stretchinnovation <it.stretchinnovation@gmail.com>
2026-03-19 21:56:03 +0200

    Update AthleticStarters.py

--- AthleticStarters.py
+++ AthleticStarters.py
@@ -73,147 +73,147 @@ match item:
         columns_to_show = ["Surname", "Firstname", "Number", "Team"]
 
 # Load Poppins font globally
-    st.markdown(
-        """
-        <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
-        <style>
-        .custom-subheader {
-            font-family: 'Poppins', sans-serif;
-            font-size: 1.5em;
-            font-weight: 800;
-            color: #000000 !important;
-            #background-color: #00ff00;
-            #background-image: linear-gradient(135deg, #EF7C19, #FCCB27) !important;
-            background-image: url("app/static/KZNA_StartList_Title_Bar.png") !important;
-            background-size: cover;
-            background-repeat: no-repeat;
-            padding: 6px 20px 12px 40px;
-            #width: 100vw;
-            text-transform: uppercase;
-            margin-top: 0em;
-            margin-bottom: 0em;
-            margin-left: 2px;
-            margin-right: 2px;
-        }
-        </style>
-        """,
-        unsafe_allow_html=True
-    ) 
-    st.markdown(
-        """
-        <style>
-        .stApp {
-            background-color: #00ff00;
-        }
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
-   
-    # Create a Styler with transparent backgrounds
-    styled = (
-        df2[columns_to_show]
-        .style
-        .hide(axis="index")
-        .set_table_styles(
-            [
-                # Table dimensions
-                {"selector": "table", "props": [
-                    ("width", "100vw"),
-                    ("max-height", "240px"),
-                    ("margin", "0px"),
-                    ("padding", "0px 0px"),
-                    ("border-collapse", "collapse")  # ensure borders collapse
-                ]},
-                # Header styling 
-                {"selector": "th", "props": [
-                    ("font-family", "Poppins"),
-                    ("font-weight", "bold"),
-                    ("color", "#ffffff"),
-                    ("background-image", "linear-gradient(135deg, #E5007E,#E5007E,#E5007E,#E5007E,#E5007E,#b00466,#7a084f, #101020) !important"),
-                    ("text-transform", "uppercase"),
-                    ("margin", "0px"),
-                    ("padding", "0px 0px"),
-                    ("border", "none"),
-                    ("font-size", "18px"),
-                    ("white-space", "nowrap")  # prevent wrapping in headers
-                ]},
-                # Cell styling
-                {"selector": "td", "props": [
-                    ("font-family", "Poppins"),
-                    ("text-transform", "uppercase"),
-                    ("background-image", "linear-gradient(150deg, #101020,#101020,#101020, #303060) !important"),
-                    ("margin", "0px"),
-                    ("padding", "0px 0px"),
-                    #("border", "none"),
-                    ("font-size", "18px"),
-                    ("white-space", "nowrap")  # prevent wrapping in headers
-                ]},
-                # Force fixed widths with !important
-                {"selector": "td.col0, th.col0", "props": [
-                    ("text-align", "center"),
-                    ("width", "60px !important"),
-                    ("padding", "0px 30px"),
-                ]},
-                {"selector": "td.col1, th.col1", "props": [
-                    ("text-align", "left"),
-                    ("width", "auto"),
-                    ("padding", "0px 30px"),
-                ]},
-                {"selector": "td.col2, th.col2", "props": [
-                    ("text-align", "left"),
-                    ("width", "auto"),
-                    ("padding", "0px 30px"),
-                ]},
-                {"selector": "td.col3, th.col3", "props": [
-                    ("text-align", "center"),
-                    ("text-align", "center"),
-                    ("width", "100px !important"),
-                    ("padding", "0px 30px"),
-                ]},
-                {"selector": "td.col4, th.col4", "props": [
-                    ("text-align", "center"),
-                    ("text-align", "center"),
-                    ("width", "100px !important"),
-                    ("padding", "0px 30px"),
-                ]},
-                #{"selector": "td.col5, th.col5", "props": [
-                #    ("text-align", "center"),
-                #    ("text-align", "center"),
-                #    ("width", "100px !important"),
-                #    ("padding", "0px 30px"),
-                #]},
-                # Alignment overrides
-                {"selector": "td.col0", "props": [("text-align", "center")]}, # Place
-                {"selector": "td.col3", "props": [("text-align", "center")]}, # Number
-                {"selector": "td.col4", "props": [("text-align", "center")]}, # Team
-                #{"selector": "td.col5", "props": [("text-align", "center")]}, # Performance
-            ]
-        )
-    )
-    # Inject CSS for centering
-    st.markdown(
-        """
-        <style>
-        /* Center the RESULTS div in the page */
-        #STARTERS {
-            display: flex;
-            justify-content: left;   /* horizontal center */
-            align-items: top;       /* vertical center */
-            flex-direction: column;    /* stack header + table */
-            min-height: 100vh;         /* take full viewport height */
-        }
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
-
-    # Render styled table and head string inside a RESULTS div
-    st.html(
-        f"""
-    <div id="STARTERS">
-        <div class="custom-subheader">{head}</div>
-        {styled.to_html()}
-    </div>
+st.markdown(
     """
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+    <style>
+    .custom-subheader {
+        font-family: 'Poppins', sans-serif;
+        font-size: 1.5em;
+        font-weight: 800;
+        color: #000000 !important;
+        #background-color: #00ff00;
+        #background-image: linear-gradient(135deg, #EF7C19, #FCCB27) !important;
+        background-image: url("app/static/KZNA_StartList_Title_Bar.png") !important;
+        background-size: cover;
+        background-repeat: no-repeat;
+        padding: 6px 20px 12px 40px;
+        #width: 100vw;
+        text-transform: uppercase;
+        margin-top: 0em;
+        margin-bottom: 0em;
+        margin-left: 2px;
+        margin-right: 2px;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+) 
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background-color: #00ff00;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+# Create a Styler with transparent backgrounds
+styled = (
+    df2[columns_to_show]
+    .style
+    .hide(axis="index")
+    .set_table_styles(
+        [
+            # Table dimensions
+            {"selector": "table", "props": [
+                ("width", "100vw"),
+                ("max-height", "240px"),
+                ("margin", "0px"),
+                ("padding", "0px 0px"),
+                ("border-collapse", "collapse")  # ensure borders collapse
+            ]},
+            # Header styling 
+            {"selector": "th", "props": [
+                ("font-family", "Poppins"),
+                ("font-weight", "bold"),
+                ("color", "#ffffff"),
+                ("background-image", "linear-gradient(135deg, #E5007E,#E5007E,#E5007E,#E5007E,#E5007E,#b00466,#7a084f, #101020) !important"),
+                ("text-transform", "uppercase"),
+                ("margin", "0px"),
+                ("padding", "0px 0px"),
+                ("border", "none"),
+                ("font-size", "18px"),
+                ("white-space", "nowrap")  # prevent wrapping in headers
+            ]},
+            # Cell styling
+            {"selector": "td", "props": [
+                ("font-family", "Poppins"),
+                ("text-transform", "uppercase"),
+                ("background-image", "linear-gradient(150deg, #101020,#101020,#101020, #303060) !important"),
+                ("margin", "0px"),
+                ("padding", "0px 0px"),
+                #("border", "none"),
+                ("font-size", "18px"),
+                ("white-space", "nowrap")  # prevent wrapping in headers
+            ]},
+            # Force fixed widths with !important
+            {"selector": "td.col0, th.col0", "props": [
+                ("text-align", "center"),
+                ("width", "60px !important"),
+                ("padding", "0px 30px"),
+            ]},
+            {"selector": "td.col1, th.col1", "props": [
+                ("text-align", "left"),
+                ("width", "auto"),
+                ("padding", "0px 30px"),
+            ]},
+            {"selector": "td.col2, th.col2", "props": [
+                ("text-align", "left"),
+                ("width", "auto"),
+                ("padding", "0px 30px"),
+            ]},
+            {"selector": "td.col3, th.col3", "props": [
+                ("text-align", "center"),
+                ("text-align", "center"),
+                ("width", "100px !important"),
+                ("padding", "0px 30px"),
+            ]},
+            {"selector": "td.col4, th.col4", "props": [
+                ("text-align", "center"),
+                ("text-align", "center"),
+                ("width", "100px !important"),
+                ("padding", "0px 30px"),
+            ]},
+            #{"selector": "td.col5, th.col5", "props": [
+            #    ("text-align", "center"),
+            #    ("text-align", "center"),
+            #    ("width", "100px !important"),
+            #    ("padding", "0px 30px"),
+            #]},
+            # Alignment overrides
+            {"selector": "td.col0", "props": [("text-align", "center")]}, # Place
+            {"selector": "td.col3", "props": [("text-align", "center")]}, # Number
+            {"selector": "td.col4", "props": [("text-align", "center")]}, # Team
+            #{"selector": "td.col5", "props": [("text-align", "center")]}, # Performance
+        ]
     )
+)
+# Inject CSS for centering
+st.markdown(
+    """
+    <style>
+    /* Center the RESULTS div in the page */
+    #STARTERS {
+        display: flex;
+        justify-content: left;   /* horizontal center */
+        align-items: top;       /* vertical center */
+        flex-direction: column;    /* stack header + table */
+        min-height: 100vh;         /* take full viewport height */
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+# Render styled table and head string inside a RESULTS div
+st.html(
+    f"""
+<div id="STARTERS">
+    <div class="custom-subheader">{head}</div>
+    {styled.to_html()}
+</div>
+"""
+)
