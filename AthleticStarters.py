33318b5d03d8414b33fe81a836100bf5681ebd5f
stretchinnovation <it.stretchinnovation@gmail.com>
2026-03-19 01:17:54 +0000

    css

--- AthleticStarters.py
+++ AthleticStarters.py
@@ -143,7 +143,7 @@ styled = (
             # Table dimensions
             {"selector": "table", "props": [
                 ("width", "100%"),          # stretch to parent width
-                ("table-layout", "auto"),   # keep proportional sizing
+                ("table-layout", "fixed"),   # keep proportional sizing
                 ("max-height", "240px"),
                 ("margin", "0px"),
                 ("padding", "0px 0px"),
